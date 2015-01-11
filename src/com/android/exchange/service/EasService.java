CyanogenMod/android_packages_apps_Exchange

Exchange: fix call wrong service when reschedule an eas ping
In Lollipop stock exchange api performs ping operations through the EasService instead of EmailSyncAdapterService
Commit 85c66ca12d7debfbbcfc0a3cc3afab1673577d50 introduced an optimization to reschedule a delayed ping request
when the last ping operation ended with an error. But the scheduleDelayedPing still create the alarm against the
EmailSyncAdapterService service, which now doesn't listen for this sort of operation.
This patch changes the alarm intent to the EasService and readd the code that starts a new request ping.

Change-Id: I16784642bf04afe6c61624dc04b5d045c86155ab
Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

src/com/android/exchange/service/EasService.java
@@ -315,6 +315,17 @@ public class EasService extends Service {
                 // if accounts disappear out from under us.
                 LogUtils.d(TAG, "Forced shutdown, killing process");
                 System.exit(-1);
+            } else if (intent.getBooleanExtra(PingSyncSynchronizer.EXTRA_START_PING, false)) {
+                LogUtils.d(TAG, "Restarting ping from alarm");
+                // We've been woken up by an alarm to restart our ping. This happens if a sync
+                // fails, rather that instantly starting the ping, we'll hold off for a few minutes.
+                final long accountId = intent.getLongExtra(
+                        PingSyncSynchronizer.EXTRA_PING_ACCOUNT_ID, -1);
+                final android.accounts.Account account =
+                        intent.getParcelableExtra(PingSyncSynchronizer.EXTRA_PING_ACCOUNT);
+
+                // End any ping and request a new one
+                mSynchronizer.pingEnd(accountId, account);
             }
         }
         return START_STICKY;

SlimSaber/packages_apps_Exchange

src/com/android/exchange/service/EasService.java
@@ -315,6 +315,17 @@ public class EasService extends Service {
                 // if accounts disappear out from under us.
                 LogUtils.d(TAG, "Forced shutdown, killing process");
                 System.exit(-1);
+            } else if (intent.getBooleanExtra(PingSyncSynchronizer.EXTRA_START_PING, false)) {
+                LogUtils.d(TAG, "Restarting ping from alarm");
+                // We've been woken up by an alarm to restart our ping. This happens if a sync
+                // fails, rather that instantly starting the ping, we'll hold off for a few minutes.
+                final long accountId = intent.getLongExtra(
+                        PingSyncSynchronizer.EXTRA_PING_ACCOUNT_ID, -1);
+                final android.accounts.Account account =
+                        intent.getParcelableExtra(PingSyncSynchronizer.EXTRA_PING_ACCOUNT);
+
+                // End any ping and request a new one
+                mSynchronizer.requestPing(accountId, account);
             }
         }
         return START_STICKY;